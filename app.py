25741838ea45b10be0b7f6e8ddecabf12ac3c838
Reinaldo Porte Peres <48915319+reinaldoperes@users.noreply.github.com>
2022-10-03 22:39:18 -0300

    feat: first recognizer

---
+++ app.py
@@ -0,0 +1,29 @@
+from email.mime import audio
+import speech_recognition as sr
+import pyttsx3
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Function to convert text to speech
+def SpeakText(command):
+  # Initialize the engine
+  engine = pyttsx3.init()
+  engine.say(command)
+  engine.runAndWait()
+ 
+# using microfone as source  
+with sr.Microphone() as source2:
+  # wait for a second to let the rocognizer adjust the energy threshold based on
+  # the surrounding noise level
+  r.adjust_for_ambient_noise(source2, duration=0.2)
+  
+  # listen for the user's input
+  audio2 = r.listen(source2)
+  
+  # using google to recognize the audio
+  myText = r.recognize_google(audio2, language="pt-BR").lower()
+  
+  print("You said: ", myText)
+  SpeakText(myText)
+  
